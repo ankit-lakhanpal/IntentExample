c0c042a53894b4cad1de253c3bdbda708567855f
Ankit <er.ankit2702@gmail.com>
2015-08-21 00:31:37 +0530

    Initial commit

---
+++ app/src/main/java/com/lakhanpal/ankit/intentexample/Apples.java
@@ -0,0 +1,53 @@
+package com.lakhanpal.ankit.intentexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class Apples extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apples);
+    }
+
+    public void onClick(View view){
+
+        Intent i=new Intent(this,bacon.class);
+
+        final EditText appleInput=(EditText) findViewById(R.id.appleTextEdit);
+        String userMgs =appleInput.getText().toString();
+        i.putExtra("appleMgs", userMgs);
+        startActivity(i);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_apples, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
